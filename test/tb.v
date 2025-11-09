dfd1f724c646e5aeeaa89bf1095a961cd3158ba5
Nathan Jones <55743963+nathancharlesjones@users.noreply.github.com>
2025-11-09 11:40:52 -0600

    Rename module instance from tt_um_example to user_project

--- test/tb.v
+++ test/tb.v
@@ -28,7 +28,7 @@ module tb ();
 `endif
 
   // Replace tt_um_example with your module name:
-  tt_um_example user_project (
+  digital_alarm_clock user_project (
 
       // Include power ports for the Gate Level test:
 `ifdef GL_TEST
